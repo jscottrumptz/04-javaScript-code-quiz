aee8ae37515ebc43cc6bf73a12cd9c2ae839ad4b
J Scott Rumptz <jscott@rumptz.tech>
2021-01-03 21:58:54 -0600

    css improvements and able to verify answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,37 @@ let pageContentEl = document.querySelector("#page-content");
 let startBtnEl = document.querySelector("#start-btn");
 let answerSectionEl = document.querySelector("#answers");
 let infoSectionEl = document.querySelector("#info");
+let correctSectionEl = document.getElementById("correct");
 let answered = false;
 let timer = 75;
-let questionNumber = 0;
 
 let beginQuiz = function(){
-    // clear main page
-    // start timer
-    // go through questions
+    // call each question object one by one
+    for (var i = 0; i < quizQuestions.length; i++) {
+        // clear info section
+        infoSectionEl.innerHTML = "";
+
+        if(timer > 0){
+
+            let pickedQuestion = quizQuestions[i];
+
+            // clear previous answers and notifications if there are any
+            answerSectionEl.innerHTML = "";
+            document.getElementById("correct-section").style.display = "none"
+
+            // asks the first question to the user
+            askQuestions(pickedQuestion);
+
+            // wait for answer
+            waitForAnswer(pickedQuestion);
+
+            // reset answer to false
+            answer = false;
+
+        } else {
+            endQuiz();
+        }
+    }
 }
 
 let endQuiz = function() {
@@ -17,37 +40,36 @@ let endQuiz = function() {
     // post high scores
 }
 
-let askQuestions = function() {
-    // call each question object one by one
-    for (var i = 0; i < quizQuestions.length; i++) {
-        // clear info section
-        infoSectionEl.innerHTML = "";
+let askQuestions = function(quizQuestions) {
 
-        // write question to document
-        document.getElementById("question").innerHTML = quizQuestions[i].question;
+    document.getElementById("question").innerHTML = quizQuestions.question;
 
-        // clear previous answers if there are any
-        answerSectionEl.innerHTML = "";
+    // generate answer buttons
+    for (var i = 0; i < quizQuestions.answers.length; i++) {
+        
+        // create an answer button
+        let answerButtonEl = document.createElement("button");
+        answerButtonEl.textContent = quizQuestions.answers[i];
 
-        // generate answer buttons
-        for (var a = 0; a < quizQuestions[i].answers.length; a++) {
-            
-
-            // create an answer button
-            let answerButtonEl = document.createElement("button");
-            answerButtonEl.textContent = quizQuestions[i].answers[a];
-
-            // set if the button is the correct answer
-            if (quizQuestions[i].answers[a] === quizQuestions[i].correctAnswer) {
-                answerButtonEl.setAttribute("data-correct", true);
-            } else {
-                answerButtonEl.setAttribute("data-correct", false);
-            }
-
-            // place the button in the answers section
-            answerSectionEl.appendChild(answerButtonEl);
+        // see if the button is the correct answer if so set data-correct to true
+        if (quizQuestions.answers[i] === quizQuestions.correctAnswer) {
+            answerButtonEl.setAttribute("data-correct", true);
+        } else {
+            answerButtonEl.setAttribute("data-correct", false);
         }
+
+        // place the button in the answers section
+        answerSectionEl.appendChild(answerButtonEl);
     }
+    
+}
+
+
+
+let waitForAnswer = function(quizQuestions) {
+    // do {
+    //     console.log("waiting...");
+    // } while((!answered) && (timer > 0))
 }
 
 let testAnswer = function(event) {
@@ -55,10 +77,12 @@ let testAnswer = function(event) {
     let targetEl = event.target;
 
     if((targetEl.matches("button")) && (targetEl.getAttribute("data-correct") === "true")) {
-        alert("You did it!");
+        document.getElementById("correct-section").style.display = "unset"
+        correctSectionEl.innerHTML = "CORRECT!";
         answered = true;
     } else if((targetEl.matches("button")) && (targetEl.getAttribute("data-correct") === "false")) {
-        alert("You failed!");
+        document.getElementById("correct-section").style.display = "unset"
+        correctSectionEl.innerHTML = "WRONG!";
         answered = true;
     }
 }
@@ -85,7 +109,7 @@ let quizQuestions = [
 
 // EVENT LISTENERS
 // start button listener to begin quiz
-startBtnEl.addEventListener("click", askQuestions);
+startBtnEl.addEventListener("click", beginQuiz);
 answerSectionEl.addEventListener("click", testAnswer);
 
 
